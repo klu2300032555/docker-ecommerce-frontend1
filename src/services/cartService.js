9905a9d160713cb254f90b71875e51c10eccd9ed
Sai Lakshmi Batta <2300032555@kluniversity.in>
2025-09-18 11:45:56 +0530

    Update cartService.js

--- src/services/cartService.js
+++ src/services/cartService.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
-const API_URL = "http://ec2-54-208-190-128.compute-1.amazonaws.com/api/cart";
+const API_URL = "http://ec2-54-226-81-84.compute-1.amazonaws.com:8081/api/cart";
 
-const API_BASE = "http://ec2-54-208-190-128.compute-1.amazonaws.com/api/cart";
+const API_BASE = "http:ec2-54-226-81-84.compute-1.amazonaws.com:8081/api/cart";
 
 export const addToCart = async (userId, productId) => {
   return await axios.post(`${API_BASE}/add`, {
