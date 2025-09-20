8ba970b7ddfc4c943c5c5e57cfb395993026fd75
Sai Lakshmi Batta <2300032555@kluniversity.in>
2025-09-20 09:49:01 +0530

    Update productService.js

--- src/services/productService.js
+++ src/services/productService.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_BASE_URL = "http://ec2-54-226-81-84.compute-1.amazonaws.com:8081/api/products";  // Backend API URL
+const API_BASE_URL = "http://3.90.214.71:8081/api/products";  // Backend API URL
 
 // Fetch products by category (computers, mobiles, laptops, pendrives, or all)
 export const getProducts = async (category = "") => {
